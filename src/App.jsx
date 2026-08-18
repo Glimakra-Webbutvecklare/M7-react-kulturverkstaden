c49325e66e7bad588cae467e57488fcd9ebced3d
Henry Bergstrom <henrybergstrom@protonmail.com>
2026-08-18 13:58:19 +0200

    workshopcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 // src/App.jsx
+
+function WorkshopCard(props) {
+
+  //console.log(props);
+
+  //return JSON.stringify(props);
+
+  // kopplr data från props till mina variabler
+  // const title = props.title;
+  // const description = props.description;
+  // const price = props.price;
+
+  return (<article className="workshop-card">
+            <img src="https://picsum.photos/200" alt="random picture"/>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+            <small>Pris: {props.price}</small>
+          </article>);
+}
+
+// Ovning
+// name, proffession, age, profile picture
+function ProfileCard() {}
+
+
 function App() {
 
-  const workshops = [{id: "keramik", title: "Keramtik", price: 100}, 
-                     {id: "virka", title: "Virka", price: 200}, 
-                     {id: "oljemalning", title: "Oljemålning", price: 350}]
+  // från databas med workshops
+  const workshops = [{id: "keramik", title: "Keramik", description: "Nybörjarkurs med fokus på skånsk rålera.",price: 100}, 
+                     {id: "virka", title: "Virke",description: "Nybörjarkurs med fokus på gottlänskt ullgarn.", price: 200}, 
+                     {id: "oljemalning", title: "Oljemålning", description: "Nybörjarkurs med fokus på smålänsk råolja.", price: 350}]
 
   const title = "Kulturverkstaden";
   // done: skapa varibel för decription
@@ -16,10 +42,9 @@ function App() {
   return (
     <main>
       <h1>{title}</h1>
-      <p>{description}</p>
-      <p>{openPlaces} platser kvar</p>
-
-      <p>{myWorkshop.title}</p>
+      <WorkshopCard title={workshops[0].title} description={workshops[0].description} price={workshops[0].price}/>
+      <WorkshopCard title={workshops[1].title} description={workshops[1].description} price={workshops[1].price}/>
+      <WorkshopCard title={workshops[2].title} description={workshops[2].description} price={workshops[2].price}/>
     </main>
   );
 }
